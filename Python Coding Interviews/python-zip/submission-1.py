2a44d81eb9f61138d82659012c59605a084f6044
sierrahls <109081974+sierrahls@users.noreply.github.com>
2026-05-31 13:57:57 -0400

    Add: python-zip - submission-1

---
+++ Python Coding Interviews/python-zip/submission-1.py
@@ -0,0 +1,18 @@
+from typing import List, Dict
+
+
+def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
+    group = {}
+    for name, score in zip(names, scores):
+        group[name] = score
+    return group
+
+"""We create an empty dict to start assigning the names and scores to.
+We iterate through both lists with zip()
+and then take the indexes and assign them to our group"""
+
+
+# do not modify below this line
+print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
