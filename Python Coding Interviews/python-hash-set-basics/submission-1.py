795806733bb4c64dece4ee4fa73138136318705f
sierrahls <109081974+sierrahls@users.noreply.github.com>
2026-06-08 18:45:09 -0400

    Add: python-hash-set-basics - submission-1

---
+++ Python Coding Interviews/python-hash-set-basics/submission-1.py
@@ -0,0 +1,32 @@
+from typing import List, Set
+
+
+def build_hash_set(keys: List[str]) -> Set[str]:
+    new_set = set(keys)
+    return new_set
+
+
+def check_keys(hash_set: Set[str], keys: List[str]) -> List[bool]:
+    empty = []
+    for key in keys: #look through keys
+        if key in hash_set: #we check if the key is in the hash set
+            empty.append(True)
+        else:
+            empty.append(False)
+
+    return empty
+    #we dont need to unpack 2 variables because that just creates 2 tuples
+
+# do not modify below this line
+
+output1 = build_hash_set(["Alice", "Bob", "Charlie"])
+print(type(output1))         # check the type of the output
+print(sorted(list(output1))) # set order is not guaranteed so we need to sort the list
+
+output2 = build_hash_set(["XY", "XX", "YY", "XY", "YX"]) 
+print(type(output2))         # check the type of the output
+print(sorted(list(output2))) # set order is not guaranteed so we need to sort the list
+
+print(check_keys({"Alice", "Bob", "Charlie"}, ["Alice", "Bob", "Charlie", "David"]))
+print(check_keys({'a', 'b', 'c'}, ['a', 'd', 'c']))
+print(check_keys({'a', 'c'}, ['d', 'c']))
